97ba44836be0e90bac0fc099cdb88c1cc03c820e
MahadFayyaz <fayazmahad675@gmail.com>
2023-08-16 12:16:38 +0500

    Commit

--- client/src/gqloperations/mutations.js
+++ client/src/gqloperations/mutations.js
@@ -13,9 +13,36 @@ mutation SigninUser($userSignin:UserSigninInput!){
     }
   }
 `
+export const EDIT_QUOTE = gql`
+  mutation EditQuote($quoteId: ID!, $quoteUpdates: QuoteEditInput!) {
+    editQuote(quoteId: $quoteId, quoteUpdates: $quoteUpdates) {
+      _id
+      name
+      length
+      width
+      height
+    }
+  }`
 
 export const CREATE_QUOTE = gql`
   mutation createQuote($quoteNew:QuoteInput!){
-    quote:createQuote(quoteNew:$quoteNew)
+    quote:createQuote(quoteNew:$quoteNew){
+      name
+      length
+      width
+      height
+      by
+    
+    }
   }
 `
+export const DELETE_USER = gql`
+  mutation DeleteUser($userId: ID!) {
+    deleteUser(userId: $userId)
+  }
+`
+export const DELETE_QUOTE = gql`
+  mutation DeleteQuote($quoteId: ID!) {
+    deleteQuote(quoteId: $quoteId)
+  }
+`;
--- resolvers.js
+++ resolvers.js
@@ -4,15 +4,16 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "./config.js";
-
+import {ObjectID} from "mongodb";
 const User = mongoose.model("User");
 const Quote = mongoose.model("Quote");
-
+// const { ObjectID } = require('mongodb');
 const resolvers = {
   Query: {
     users: async () => await User.find({}),
     user: async (_, { _id }) => await User.findOne({ _id }),
     quotes: async () => await Quote.find({}).populate("by", "_id firstName"),
+    quote: async(_, {_id}) => await Quote.findOne({ _id }),
     iquote: async (_, { by }) => await Quote.find({ by }),  
     myprofile: async (_, args, { userId }) => {
       if (!userId) throw new Error("You must be logged in");
@@ -36,10 +37,10 @@ const resolvers = {
       });
       return await newUser.save();
     },
-    signinUser: async (_, { userSignin }) => {
+    signinUser: async (_, { userSignin }) => {3
       const user = await User.findOne({ email: userSignin.email });
       if (!user) {
-        throw new Error("User dosent exists with that email");
+        throw new Error("User12 dosent exists with that email");
       }
       const doMatch = await bcrypt.compare(userSignin.password, user.password);
       if (!doMatch) {
@@ -48,16 +49,59 @@ const resolvers = {
       const token = jwt.sign({ userId: user._id }, JWT_SECRET);
       return { token };
     },
+    deleteUser: async (_, { userId }) => {
+      try {
+        // Find the user by ID and deletex
+        const deletedUser = await User.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+          throw new Error('User not found');
+        }
+
+        return true; // Return true to indicate successful deletion
+      } catch (error) {
+        console.error('Error deleting user:', error);
+        return false; // Return false in case of errors
+      }
+    },
     createQuote: async (_, { quoteNew }, { userId }) => {
       //    if(!userId) throw new Error("You must be logged in")
       const newQuote = new Quote({
+        _id: new ObjectID(),
         ...quoteNew,
         by: userId,
       });
      return await newQuote.save();
-    
-   
     },
+    editQuote: async (_, { quoteId, quoteUpdates }) => {
+      try {
+        // Check if the provided quoteId is valid
+        const quote = await Quote.findById(quoteId);
+        if (!quote) {
+          throw new Error("Quote not found");
+        }
+
+        // Update the quote with the provided updates
+        Object.assign(quote, quoteUpdates);
+        
+        const updatedQuote = await quote.save();
+        return updatedQuote;
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    },
+    deleteQuote: async (_, { quoteId }) => {
+      try {
+        // Delete the quote by its ID from the database
+        const result = await Quote.findByIdAndDelete(quoteId);
+        return result !== null; // Return true if delete was successful, false if not
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+
   },
 };
 
